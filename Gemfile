7374138821f8d86ef6e783d1e0960755798468d1
Charles Flood <charlesFlood3@gmail.com>
2013-09-18 14:24:49 -0700

    Added static home and help pages. Also added devise gem and created user signin, login, etc.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
+gem 'devise'
+
 group :development do
   gem 'sqlite3', '1.3.7'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 DempApp::Application.routes.draw do
+
+
+  devise_for :users
+  root :to => "static_pages#home"
+
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
