fc21cb8ca8c7c8e0545ae0e1a34523b00883ea34
Yaniv <yanivcohen@gmail.com>
2013-10-06 22:36:35 -0700

    all users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,31 @@
 class UsersController < ApplicationController
-   before_action :signed_in_user, only: [:edit, :update]
+   before_action :signed_in_user, only: [:index, :edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
     @user = User.find(params[:id])
   end
+  
+ 
+ def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
 
   def new
   	 @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
    def update
@@ -43,7 +59,24 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
 
 end
