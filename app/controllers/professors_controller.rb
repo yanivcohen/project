e8e382554aeb77bf27b33d15ce98d97090491d22
Charles Flood <charlesFlood3@gmail.com>
2013-10-11 10:18:39 -0700

    Changed Professor controller to fix bug that saved nil values to all but the ID field.

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,36 +1,37 @@
 class ProfessorsController < ApplicationController
   
   def new
-  	@professor=Professor.new
+    @professor=Professor.new
   end
   
- 	def create
-  	  @professor=Professor.new(params[ professor_params])
-    	if @professor.save
-    	    flash[:success] = "Professor saved successfuly"
-          redirect_to new_professor_path
-    	else
-      		render 'new'
-    	end	
+  def create
+    @professor=Professor.new(professor_params)
+    if @professor.save
+      flash[:success] = "Professor saved successfuly"
+      redirect_to new_professor_path
+    else
+      render 'new'
+    end	
   end 
 
   def index
- 	@search = Professor.search do
-    fulltext params[:search]
-  end
-   @matchProfessor = @search.results
+    @search = Professor.search do
+      fulltext params[:search]
+    end
+    @matchProfessor = @search.results
   end 
 
   
 
   def show
+    @professor=Professor.find(params[:id])
   end
   
 
   private
- 	def professor_params
-      params.require(:professor).permit(:firstname, :lastname, :department)
- 	end
 
+    def professor_params
+      params.require(:professor).permit(:firstname, :lastname, :department)
+    end
 
 end
