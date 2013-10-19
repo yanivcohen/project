d15f9e8689d1be99d8ff8a0b1a12ecc7e7c5e7ed
Yaniv <yanivcohen@gmail.com>
2013-10-18 20:33:45 -0700

    "deleted the migrate file professors"

--- db/migrate/20131018022840_drop_professors_table.rb
+++
@@ -1,9 +0,0 @@
-class DropProfessorsTable < ActiveRecord::Migration
-  def up
-    drop_table :professors
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018023710) do
+ActiveRecord::Schema.define(version: 20131019031923) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "edition"
+    t.string   "author"
+    t.string   "isbn"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "professors", force: true do |t|
     t.string   "firstname"
