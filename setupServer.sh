a559da0099dc6cc955a15ca94358ed26cc21dd6c
Charles Flood <charlesFlood3@gmail.com>
2013-12-09 16:05:57 -0800

    Fixed bug in professor rating. Added script to bootstrap installation on new machines. Updated Readme file accordingly.

--- setupServer.sh
+++ setupServer.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 #This script is designed to start the ruby server, installing software if needs be.
 
 apt-get update
 
 echo "installing required software packages..."
-apt-get -y install curl git nodejs postgresql-9.1 postgresqp-server-dev-9.1
+apt-get -y install curl git nodejs postgresql-9.1 postgresql-server-dev-9.1
 
-#Make sure we have ruby
+#Make sure we have ruby.
 if command -v ruby >/dev/null ;then
     echo "Ruby is already installed"
 else
     echo "Ruby not installed."
 
     echo "Installing RVM."
-    curl -L https://get.rvm.io >> rvmInstall.sh
+    curl -L https://get.rvm.io > rvmInstall.sh
     chmod +x rvmInstall.sh
-    ./rvmInstall.sh
+    ./rvmInstall.shrail
     source ~/.rvm/scripts/rvm
+    source /etc/profile.d/rvm.sh
     echo "Intstalling ruby 2.0.0. This may take some time..."
     rvm install ruby-2.0.0
 fi
