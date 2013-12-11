999581f258aaf5f55e24e10e3a231f12371bb169
Charles Flood <charlesFlood3@gmail.com>
2013-12-10 20:56:59 -0800

    Modified setup script, preped for demo.

--- setupServer.sh
+++ setupServer.sh
@@ -2,25 +2,26 @@
 
 #This script is designed to start the ruby server, installing software if needs be.
 
-apt-get update
+sudo apt-get update
 
 echo "installing required software packages..."
-apt-get -y install curl git nodejs postgresql-9.1 postgresql-server-dev-9.1
+sudo apt-get -y install curl git nodejs postgresql-9.1 postgresql-server-dev-9.1
 
 #Make sure we have ruby.
 if command -v ruby >/dev/null ;then
     echo "Ruby is already installed"
 else
     echo "Ruby not installed."
-
     echo "Installing RVM."
     curl -L https://get.rvm.io > rvmInstall.sh
     chmod +x rvmInstall.sh
-    ./rvmInstall.shrail
+    ./rvmInstall.sh
     source ~/.rvm/scripts/rvm
     source /etc/profile.d/rvm.sh
+    echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
     echo "Intstalling ruby 2.0.0. This may take some time..."
     rvm install ruby-2.0.0
+    
 fi
 
 
